b0682f3725f716d4234ab77e4079aece19b966fb
Kyrylo <kyrylo.mm@gmail.com>
2023-10-29 23:31:04 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
-
+// Загальна функція для генерування кольору
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  };
+  // Змінні кнопок старту та стопу
+  const startSwitcherButton = document.querySelector('[data-start]');
+  const stopSwitcherButton = document.querySelector('[data-stop]');
+//   Змінна відображення номера кольору
+  const colorDisplay = document.querySelector('[color-display]');
+// Змінна інтервалу
+let randomTimer = null;
+// Функція логіки кнопки старт
+  function clickStart() {
+    randomTimer = setInterval(() => {
+      let randomColor = getRandomHexColor();
+      document.body.style.backgroundColor = randomColor;
+      colorDisplay.textContent = randomColor;
+    }, 1000);
+    startSwitcherButton.setAttribute('disabled', '');
+  };
+  startSwitcherButton.addEventListener('click', clickStart);
+  // Функція логіки кнопки стоп
+  function clickStop() {
+    clearInterval(randomTimer);
+    startSwitcherButton.removeAttribute('disabled');
+  };
+  stopSwitcherButton.addEventListener('click', clickStop);
